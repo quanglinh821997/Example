d1206e2f5ff50f164ca5e7f6454156bc242d54d8
Liem Q. Pham <phamquangliem96@gmail.com>
2022-08-13 23:05:56 +0700

    refactor code

--- src/main/java/com/codespring/cron/TutorialScheduler.java
+++ src/main/java/com/codespring/cron/TutorialScheduler.java
@@ -1,6 +1,5 @@
 package com.codespring.cron;
 
-import com.codespring.controller.EmployeeController;
 import com.codespring.model.Employee;
 import com.codespring.services.EmailService;
 import com.codespring.services.EmployeeServices;
@@ -8,33 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 @Component
 public class TutorialScheduler {
 
-    @Autowired private EmailService emailService;
+    @Autowired
+    private EmailService emailService;
     @Autowired
     EmployeeServices employeeServices;
 
     @Scheduled(fixedRate = 120000L)
     public void cheduleTaskUsingCronExpression() {
-            List<Employee> employeeList = employeeServices.getAllEmployee();
-            if (employeeList == null) {
-                System.out.println(new Date().toString() + " Employee List is Empty...");
-            } else {
-                System.out.println(new Date().toString() + " Employee List is not Empty and size is: " + employeeList.size());
-                var empList = employeeServices.getEmployeeByMinutes();
-
-                if (empList != null) {
-                    for (Employee emp:empList) {
-                        emailService.sendSimpleMail("quanglinh238297@gmail.com","Hello" + emp.getName(), "Demo");
-                    }
-                }
-
+        var employees = employeeServices.getEmployeeByMinutes();
+        if (employees.isEmpty()) {
+            System.out.println(new Date() + " Employee List is Empty...");
+        } else {
+            for (Employee employee : employees) {
+                emailService.sendSimpleMail("quanglinh238297@gmail.com", "Hello " + employee.getName(), "Demo");
             }
+        }
     }
 }
 
